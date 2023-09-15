93a34deac1033a5dc13f9abb66c065175af8d360
Menkzy <iammenkz@gmail.com>
2023-09-15 12:48:32 +1000

    initial upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Menkzy @ Discord'
+description 'Allows streaming of new/current build blips for b2944. This includes two versions, one without an armour/health bar minimap & one with it.'
+repository   'https://github.com/menkzy/fivem-blips'
+
+dependencies {
+	'/server:6683', -- Current Recommended
+    '/onesync', -- unsure if this is needed but why not
+    '/gameBuild:2944', -- mp2023_01	San Andreas Mercenaries
+}
+
+lua54 'yes'
